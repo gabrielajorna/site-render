6986e4c6831955b499d468520df03ab9c0ef27dc
Gabriela-Cassoli <gabrielajorna@gmail.com>
2024-04-05 17:25:19 -0300

    atualizando com scraping

--- app.py
+++ app.py
@@ -1,26 +1,80 @@
 import os
 import requests
+import bs4
 from flask import Flask, request
 from dotenv import load_dotenv
 load_dotenv()
+import scraping
+import gspread
+from oauth2client.service_account import ServiceAccountCredentials
 
-
-app= Flask(__name__)
 BOT_TOKEN= os.environ['TELEGRAM_BOT_TOKEN']
 
+#INTEGRAÇÃO COM GOOGLE SHEETS
+conteudo_credenciais = os.environ["GOOGLE_SHEETS_CREDENTIALS"]
+arquivo_credenciais = "algoritmo-aula01-822d22859c10.json"
+with open(arquivo_credenciais, mode="w") as arquivo:
+  arquivo.write(conteudo_credenciais)
+conta = ServiceAccountCredentials.from_json_keyfile_name(arquivo_credenciais)
+api = gspread.authorize(conta)
+planilha = api.open_by_key("1-JGRKA-HX_vFHZB3aQJJq9cGnaOdfZbMMA8eg1qR7NY")
+sheet = planilha.worksheet("Página2")
+
+app= Flask(__name__)
+
+#FORMATANDO AS NOTÍCIAS RASPADAS PARA HTML 
+def formata_noticias(editoria, noticias):
+  html = f"Que tal incluir essas notícias na sua próxima pauta de {editoria}:\n\n"
+  for materia in noticias:
+    html += f'- <a href="{materia["url"]}">{materia["titulo"]}</a>\n'
+  return html
+
+#INICIANDO AS PÁGINAS DO SITE
 @app.route("/")
 def index():
   return "Olá <b>Tudo bem?</b>"
-  
-@app.route("/teste")
-def teste():
-  return 'Olá essa página é um <b>teste</b>!'
 
 @app.route('/telegram', methods=['POST'])
-def telegram_update():
-  update=request.json
-  url_envio_mensagem = f'https://api.telegram.org/bot{BOT_TOKEN}/sendMessage'
-  chat_id=update['message']['chat']['id']
-  mensagem= {'chat_id': chat_id, 'text':'mensagem <b>recebida</b>!', 'parse_mode':'HTML'}
-  return 'ok'
+def telegram_update(request):
+    dados = request.json()
+    url_envia_mensagem = f"https://api.telegram.org/bot{token}/sendMessage"
+    ultimo_id_processado = int(sheet.get("A1")[0][0])
+    print(f"Começando a partir do update_id = {ultimo_id_processado}")
 
+    for update in dados["result"]:
+        if "message" not in update:  # Não é uma mensagem, pula
+            continue
+        if update["update_id"] <= ultimo_id_processado:
+            print(f"Pulando atualização {update['update_id']} - já processada")
+            continue  # Pula para próximo update caso seja igual
+
+        first_name = update["message"]["from"]["first_name"]
+        texto = update["message"]["text"]
+        chat_id = update["message"]["chat"]["id"]
+        print(f"Mensagem de {first_name}: {texto}")
+
+        if texto == "/start":
+            resposta = "Bem-vindo(a), eu sou o Notifiquei.bot e vou te mostrar que notícias do Terceiro Setor cabem em qualquer pauta jornalística. Vamos começar? Escolha uma das editorias: /educacao, /economia, /esporte "
+        elif texto == '/educacao':
+            materias_insper = scraping.raspar_insper(headers, url_insper)
+            materias_peninsula = scraping.raspar_peninsula(headers, url_peninsula)
+            resposta = formata_noticias("Educação", materias_insper + materias_peninsula)
+            print(texto)
+        elif texto == '/economia':
+            materias_dara = scraping.raspar_dara(headers, url_dara)
+            materias_igarape = scraping.raspar_igarape(headers, url_igarape)
+            resposta = formata_noticias("Economia", materias_dara + materias_neymarjr)
+            print(texto)
+        elif texto == '/esporte':
+            materias_ee = scraping.raspar_ee(headers, url_ee)
+            materias_neymarjr = scraping.raspar_neymarjr(headers, url_neymarjr)
+            resposta = formata_noticias("Esportes", materias_ee + materias_neymarjr)
+            print(texto)
+        mensagem = {"chat_id": chat_id, "text": resposta, 'parse_mode': 'HTML'}
+        resultado = requests.post(url_envia_mensagem, data=mensagem)
+
+    return "ok"  # Retornar algo como ok ao final da função
+
+def adicionar_na_planilha(chat_id, texto):
+  planilha.append_row([chat_id, texto])
+  print('Mensagem armazenada com sucesso!')
---
+++ scraping.py
@@ -0,0 +1,104 @@
+import requests
+import bs4
+
+#INICIANDO AS RASPAGENS DE EDUCAÇÃO
+headers = {
+    'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.12; rv:55.0) Gecko/20100101 Firefox/55.0',
+}
+url_insper = "https://www.insper.edu.br/imprensa/"
+
+def raspar_insper(headers, url_insper):
+  result_insper = requests.get(url_insper,  headers = headers)
+  soup_insper = bs4.BeautifulSoup(result_insper.text, 'html.parser')
+  links_insper = soup_insper.find_all('div', { 'class': '_thumbnail__descricao no-paddingM' })
+  noticias_insper = []
+  for link in links_insper:
+    link_insper = link.find('a').get('href')
+    titulo = link.find('a').text.strip()
+    noticias_insper.append([titulo,link_insper])
+  return noticias_insper
+
+raspar_insper(headers, url_insper)
+
+headers = {
+    'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.12; rv:55.0) Gecko/20100101 Firefox/55.0',
+}
+url_peninsula = "https://www.institutopeninsula.org.br/noticias/"
+
+def raspar_peninsula(headers, url_peninsula):
+  result_peninsula = requests.get(url_peninsula,  headers = headers)
+  soup_peninsula = bs4.BeautifulSoup(result_peninsula.text, 'html.parser')
+  links_peninsula = soup_peninsula.find_all('div', { 'class': 'item' })
+  noticias_peninsula = []
+  for link in links_peninsula:
+    link_peninsula = link.find('a').get('href')
+    titulo = link.find('a', {'class':'item__link'}).text.strip()
+    noticias_peninsula.append([titulo,link_peninsula])
+  return noticias_peninsula
+
+raspar_peninsula(headers, url_peninsula)
+
+#INICIANDO AS RASPAGENS DE ECONOMIA
+headers = {
+    'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.12; rv:55.0) Gecko/20100101 Firefox/55.0',
+}
+url_igarape = "https://igarape.org.br/press-releases/"
+
+def raspar_igarape(headers, url_igarape):
+    noticias_igarape = []
+    response = requests.get(url_igarape, headers=headers)
+    soup = bs4.BeautifulSoup(response.content, 'html.parser')
+    conteudo = soup.find_all('div', {'class': 'uc_post_grid_style_one_item ue_post_grid_item ue-item'})
+
+    for item in conteudo:
+        titulo_link = item.find('a', {'class': 'uc_post_grid_style_one_image'})
+        if titulo_link:
+            # Ajuste aqui: Buscar diretamente o 'div' com classe 'uc_post_title' dentro de 'item', não de 'titulo_link'
+            div_titulo = item.find('div', class_='uc_post_title')
+            if div_titulo:
+                titulo = div_titulo.text.strip()
+                link = titulo_link['href']
+                # Verifica se o link já existe na lista para evitar duplicatas
+                if not any(noticia[1] == link for noticia in noticias_igarape):
+                    noticias_igarape.append([titulo, link])
+    return noticias_igarape
+
+raspar_igarape(headers, url_igarape)
+
+headers = {
+    'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.12; rv:55.0) Gecko/20100101 Firefox/55.0',
+}
+
+url_dara = "https://dara.org.br/informe-se/noticias/"
+
+def raspar_dara(headers,url_dara):
+  result_dara = requests.get(url_dara,  headers = headers)
+  soup_dara = bs4.BeautifulSoup(result_dara.text, 'html.parser')
+  links_dara = soup_dara.find_all('div', { 'class': 'category-post-conteudo' })
+  noticias_dara = []
+  for link in links_dara:
+    link_dara = link.find('a').get('href')
+    titulo = link.find('a').text.strip()
+    noticias_dara.append([titulo,link_dara])
+  return noticias_dara
+
+raspar_dara(headers,url_dara)
+
+#INICIANDO AS RASPAGENS DE ESPORTES
+headers = {
+    'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.12; rv:55.0) Gecko/20100101 Firefox/55.0',
+}
+url_ee = "https://esporteeducacao.org.br/noticias/"
+
+def raspar_ee(headers,url_ee):
+  result_ee = requests.get(url_ee,  headers = headers)
+  soup_ee = bs4.BeautifulSoup(result_ee.text, 'html.parser')
+  links_ee = soup_ee.find_all('div', { 'class': 'uc_post_title' })
+  noticias_ee = []
+  for link in links_ee:
+    link_ee = link.find('a').get('href')
+    titulo = link.find('div', { 'class': 'ue_p_title' }).text.strip()
+    noticias_ee.append([titulo,link_ee])
+  return noticias_ee
+
+raspar_ee(headers,url_ee)
