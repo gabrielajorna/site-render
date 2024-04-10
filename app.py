99d31a9b48f293024be1d1594c5b22bf4a6fa236
Gabriela-Cassoli <gabrielajorna@gmail.com>
2024-04-10 13:56:14 -0300

    agora vai

--- app.py
+++ app.py
@@ -72,8 +72,6 @@ def telegram_update():
     update = request.json
     url_envio_mensagem = f'https://api.telegram.org/bot{BOT_TOKEN}/sendMessage'
     chat_id = update['message']['chat']['id']
-    mensagem = {"chat_id": chat_id, "text": resposta, 'parse_mode': 'HTML'}
-    requests.post(url_envio_mensagem, data=mensagem)
 
     for update in dados["result"]:
         if "message" not in update:  # Não é uma mensagem, pula
